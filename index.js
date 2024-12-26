1655f27844591aa2851cad048001576862eed11e
sychyy <ydaanewww@gmail.com>
2024-12-26 18:45:50 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -60,158 +60,177 @@ figlet('SYCH', { font: 'Big' }, (err, data) => {
 	}
 	console.log(chalk.green(data));  // Display the banner in green
 });
-async function startSychBot() {
-	const { state, saveCreds } = await useMultiFileAuthState('nazedev');
-	const { version, isLatest } = await fetchLatestBaileysVersion();
-	const level = pino({ level: 'silent' })
-	
-	const getMessage = async (key) => {
-		if (store) {
-			const msg = await store.loadMessage(key.remoteJid, key.id);
-			return msg?.message || ''
-		}
-		return {
-			conversation: 'Halo Saya sychy Bot'
-		}
-	}
-	
-	const sych = WAConnection({
-		isLatest,
-		logger: level,
-		getMessage,
-		syncFullHistory: true,
-		maxMsgRetryCount: 15,
-		msgRetryCounterCache,
-		retryRequestDelayMs: 10,
-		defaultQueryTimeoutMs: 0,
-		printQRInTerminal: !pairingCode,
-		browser: Browsers.ubuntu('Chrome'),
-		generateHighQualityLinkPreview: true,
-		transactionOpts: {
-			maxCommitRetries: 10,
-			delayBetweenTriesMs: 10,
-		},
-		appStateMacVerification: {
-			patch: true,
-			snapshot: true,
-		},
-		auth: {
-			creds: state.creds,
-			keys: makeCacheableSignalKeyStore(state.keys, level),
-		},
-	})
-	
-	if (pairingCode && !sych.authState.creds.registered) {
-		let phoneNumber;
-		async function getPhoneNumber() {
-			phoneNumber = await question('Please type your WhatsApp number : ');
-			phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
-			
-			if (!parsePhoneNumber(phoneNumber).valid && phoneNumber.length < 6) {
-				console.log(chalk.bgBlack(chalk.redBright('Start with your Country WhatsApp code') + chalk.whiteBright(',') + chalk.greenBright(' Example : 62xxx')));
-				await getPhoneNumber()
-			}
-		}
-		
-		setTimeout(async () => {
-			await getPhoneNumber()
-			await exec('rm -rf ./nazedev/*')
-			let code = await sych.requestPairingCode(phoneNumber);
-			console.log(`Your Pairing Code : ${code}`);
-		}, 3000)
-	}
-	
-	store.bind(sych.ev)
-	
-	await Solving(sych, store)
-	
-	sych.ev.on('creds.update', saveCreds)
-	
-	sych.ev.on('connection.update', async (update) => {
-		const { connection, lastDisconnect, receivedPendingNotifications } = update
-		if (connection === 'close') {
-			const reason = new Boom(lastDisconnect?.error)?.output.statusCode
-			if (reason === DisconnectReason.connectionLost) {
-				console.log('Connection to Server Lost, Attempting to Reconnect...');
-				startSychBot()
-			} else if (reason === DisconnectReason.connectionClosed) {
-				console.log('Connection closed, Attempting to Reconnect...');
-				startSychBot()
-			} else if (reason === DisconnectReason.restartRequired) {
-				console.log('Restart Required...');
-				startSychBot()
-			} else if (reason === DisconnectReason.timedOut) {
-				console.log('Connection Timed Out, Attempting to Reconnect...');
-				startSychBot()
-			} else if (reason === DisconnectReason.badSession) {
-				console.log('Delete Session and Scan again...');
-				startSychBot()
-			} else if (reason === DisconnectReason.connectionReplaced) {
-				console.log('Close current Session first...');
-				startSychBot()
-			} else if (reason === DisconnectReason.loggedOut) {
-				console.log('Scan again and Run...');
-				exec('rm -rf ./nazedev/*')
-				process.exit(1)
-			} else if (reason === DisconnectReason.Multidevicemismatch) {
-				console.log('Scan again...');
-				exec('rm -rf ./nazedev/*')
-				process.exit(0)
-			} else {
-				sych.end(`Unknown DisconnectReason : ${reason}|${connection}`)
-			}
-		}
-		if (connection == 'open') {
-			console.log('Connected to : ' + JSON.stringify(sych.user, null, 2));
-			let botNumber = await sych.decodeJid(sych.user.id);
-			if (db.set[botNumber] && !db.set[botNumber]?.join) {
-				if (global.my.gc.length > 0 && global.my.gc.includes('whatsapp.com')) {
-					await sych.groupAcceptInvite(global.my.gc?.split('https://chat.whatsapp.com/')[1]).then(async grupnya => {
-						await sych.chatModify({ archive: true }, grupnya, [])
-						db.set[botNumber].join = true
-					});
-				}
-			}
-		}
-		if (receivedPendingNotifications == 'true') {
-			console.log('Please wait About 1 Minute...')
-			sych.ev.flush()
-		}
-	});
-	
-	sych.ev.on('contacts.update', (update) => {
-		for (let contact of update) {
-			let id = sych.decodeJid(contact.id)
-			if (store && store.contacts) store.contacts[id] = { id, name: contact.notify }
-		}
-	});
-	
-	sych.ev.on('call', async (call) => {
-		let botNumber = await sych.decodeJid(sych.user.id);
-		if (db.set[botNumber].anticall) {
-			for (let id of call) {
-				if (id.status === 'offer') {
-					let msg = await sych.sendMessage(id.from, { text: `Saat Ini, Kami Tidak Dapat Menerima Panggilan ${id.isVideo ? 'Video' : 'Suara'}.\nJika @${id.from.split('@')[0]} Memerlukan Bantuan, Silakan Hubungi Owner :)`, mentions: [id.from]});
-					await sych.sendContact(id.from, global.owner, msg);
-					await sych.rejectCall(id.id, id.from)
-				}
-			}
-		}
-	});
-	
-	sych.ev.on('groups.update', async (update) => {
-		await GroupUpdate(sych, update, store);
-	});
-	
-	sych.ev.on('group-participants.update', async (update) => {
-		await GroupParticipantsUpdate(sych, update, store);
-	});
-	
-	sych.ev.on('messages.upsert', async (message) => {
-		await MessagesUpsert(sych, message, store);
-	});
 
-	return sych
+async function startSychBot() {
+    let dots = '';
+    let isLoadingDisplayed = false;
+
+    const waitingMessage = () => {
+        // Cek jika loading sudah ditampilkan, agar tidak spam
+        if (isLoadingDisplayed) return;
+
+        isLoadingDisplayed = true; // Tampilkan loading pertama kali
+
+        setInterval(() => {
+            dots += '.';
+            if (dots.length > 3) dots = ''; // Reset titik setelah 3 titik
+            process.stdout.write(chalk.yellow(`Menunggu pesan${dots}   \r`));  // Update pada baris yang sama
+        }, 500); // Interval 500ms (setiap setengah detik)
+    };
+
+    waitingMessage(); // Memulai animasi menunggu pesan...
+
+    const { state, saveCreds } = await useMultiFileAuthState('nazedev');
+    const { version, isLatest } = await fetchLatestBaileysVersion();
+    const level = pino({ level: 'silent' });
+
+    const getMessage = async (key) => {
+        if (store) {
+            const msg = await store.loadMessage(key.remoteJid, key.id);
+            return msg?.message || '';
+        }
+        return {
+            conversation: 'Halo Saya sychy Bot'
+        };
+    };
+
+    const sych = WAConnection({
+        isLatest,
+        logger: level,
+        getMessage,
+        syncFullHistory: true,
+        maxMsgRetryCount: 15,
+        msgRetryCounterCache,
+        retryRequestDelayMs: 10,
+        defaultQueryTimeoutMs: 0,
+        printQRInTerminal: !pairingCode,
+        browser: Browsers.ubuntu('Chrome'),
+        generateHighQualityLinkPreview: true,
+        transactionOpts: {
+            maxCommitRetries: 10,
+            delayBetweenTriesMs: 10,
+        },
+        appStateMacVerification: {
+            patch: true,
+            snapshot: true,
+        },
+        auth: {
+            creds: state.creds,
+            keys: makeCacheableSignalKeyStore(state.keys, level),
+        },
+    });
+
+    if (pairingCode && !sych.authState.creds.registered) {
+        let phoneNumber;
+        async function getPhoneNumber() {
+            phoneNumber = await question('Please type your WhatsApp number : ');
+            phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
+            
+            if (!parsePhoneNumber(phoneNumber).valid && phoneNumber.length < 6) {
+                console.log(chalk.bgBlack(chalk.redBright('Start with your Country WhatsApp code') + chalk.whiteBright(',') + chalk.greenBright(' Example : 62xxx')));
+                await getPhoneNumber()
+            }
+        }
+        
+        setTimeout(async () => {
+            await getPhoneNumber()
+            await exec('rm -rf ./nazedev/*')
+            let code = await sych.requestPairingCode(phoneNumber);
+            console.log(`Your Pairing Code : ${code}`);
+        }, 3000)
+    }
+
+    store.bind(sych.ev)
+
+    await Solving(sych, store)
+
+    sych.ev.on('creds.update', saveCreds)
+
+    sych.ev.on('connection.update', async (update) => {
+        const { connection, lastDisconnect, receivedPendingNotifications } = update
+        if (connection === 'close') {
+            const reason = new Boom(lastDisconnect?.error)?.output.statusCode
+            if (reason === DisconnectReason.connectionLost) {
+                console.log('Connection to Server Lost, Attempting to Reconnect...');
+                startSychBot()
+            } else if (reason === DisconnectReason.connectionClosed) {
+                console.log('Connection closed, Attempting to Reconnect...');
+                startSychBot()
+            } else if (reason === DisconnectReason.restartRequired) {
+                console.log('Restart Required...');
+                startSychBot()
+            } else if (reason === DisconnectReason.timedOut) {
+                console.log('Connection Timed Out, Attempting to Reconnect...');
+                startSychBot()
+            } else if (reason === DisconnectReason.badSession) {
+                console.log('Delete Session and Scan again...');
+                startSychBot()
+            } else if (reason === DisconnectReason.connectionReplaced) {
+                console.log('Close current Session first...');
+                startSychBot()
+            } else if (reason === DisconnectReason.loggedOut) {
+                console.log('Scan again and Run...');
+                exec('rm -rf ./nazedev/*')
+                process.exit(1)
+            } else if (reason === DisconnectReason.Multidevicemismatch) {
+                console.log('Scan again...');
+                exec('rm -rf ./nazedev/*')
+                process.exit(0)
+            } else {
+                sych.end(`Unknown DisconnectReason : ${reason}|${connection}`)
+            }
+        }
+        if (connection == 'open') {
+            console.log('Connected to : ' + JSON.stringify(sych.user, null, 2));
+            let botNumber = await sych.decodeJid(sych.user.id);
+            if (db.set[botNumber] && !db.set[botNumber]?.join) {
+                if (global.my.gc.length > 0 && global.my.gc.includes('whatsapp.com')) {
+                    await sych.groupAcceptInvite(global.my.gc?.split('https://chat.whatsapp.com/')[1]).then(async grupnya => {
+                        await sych.chatModify({ archive: true }, grupnya, [])
+                        db.set[botNumber].join = true
+                    });
+                }
+            }
+        }
+        if (receivedPendingNotifications == 'true') {
+            console.log('Please wait About 1 Minute...')
+            sych.ev.flush()
+        }
+    });
+
+    sych.ev.on('contacts.update', (update) => {
+        for (let contact of update) {
+            let id = sych.decodeJid(contact.id)
+            if (store && store.contacts) store.contacts[id] = { id, name: contact.notify }
+        }
+    });
+
+    sych.ev.on('call', async (call) => {
+        let botNumber = await sych.decodeJid(sych.user.id);
+        if (db.set[botNumber].anticall) {
+            for (let id of call) {
+                if (id.status === 'offer') {
+                    let msg = await sych.sendMessage(id.from, { text: `Saat Ini, Kami Tidak Dapat Menerima Panggilan ${id.isVideo ? 'Video' : 'Suara'}.\nJika @${id.from.split('@')[0]} Memerlukan Bantuan, Silakan Hubungi Owner :)`, mentions: [id.from]});
+                    await sych.sendContact(id.from, global.owner, msg);
+                    await sych.rejectCall(id.id, id.from)
+                }
+            }
+        }
+    });
+
+    sych.ev.on('groups.update', async (update) => {
+        await GroupUpdate(sych, update, store);
+    });
+
+    sych.ev.on('group-participants.update', async (update) => {
+        await GroupParticipantsUpdate(sych, update, store);
+    });
+
+    sych.ev.on('messages.upsert', async (message) => {
+        await MessagesUpsert(sych, message, store);
+    });
+
+    return sych
 }
 
 startSychBot()
